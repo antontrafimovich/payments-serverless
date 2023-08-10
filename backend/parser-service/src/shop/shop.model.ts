b270e98eb0499544876bd741f0508f90c0f78361
Anton Trafimovich <tonytrof@gmail.com>
2023-08-10 12:21:46 +0200

    fix: filter empty items from notion response.

---
+++ backend/parser-service/src/shop/shop.model.ts
@@ -0,0 +1,4 @@
+export type Shop = {
+  address: string;
+  type: string;
+};
--- backend/parser-service/src/shop/shop.service.ts
+++ backend/parser-service/src/shop/shop.service.ts
@@ -1,14 +1,24 @@
+import { Shop } from "./shop.model";
+
 export const service = (db: any) => {
   return {
-    getAll: async () => {
+    getAll: async (): Promise<Shop[]> => {
       const response = await db.query(process.env.NOTION_DATABASE);
 
-      const data = response.results.map((item: any) => {
-        return {
-          Address: item.properties.Address.title[0].text.content,
-          Type: item.properties.Type.select.name,
-        };
-      });
+      const data = response.results
+        .map((item: any) => {
+          const [title] = item.properties.Address.title;
+
+          if (!title.text) {
+            return null;
+          }
+
+          return {
+            address: item.properties.Address.title[0].text.content,
+            type: item.properties.Type.select.name,
+          };
+        })
+        .filter((item: any) => item !== null);
 
       return data;
     },
