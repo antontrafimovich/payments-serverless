7e80134735f9e419a77ebfc0d9e6b329e7d1cbab
Anton Trafimovich <tonytrof@gmail.com>
2023-08-12 16:21:31 +0200

    feat: change report name.

--- clients/web/src/src/App.tsx
+++ clients/web/src/src/App.tsx
@@ -2,14 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <form
-      action={`${import.meta.env.VITE_API_URL as string}/report`}
-      encType="multipart/form-data"
-      method="post"
-    >
-      <input type="file" name="report" />
-      <button type="submit">Send file</button>
-    </form>
+    <>
+      <h1>Hola Liza Report</h1>
+      <form
+        action={`${import.meta.env.VITE_API_URL as string}/report`}
+        encType="multipart/form-data"
+        method="post"
+      >
+        <input type="file" name="report" />
+        <button type="submit">Send file</button>
+      </form>
+    </>
   );
 }
 
