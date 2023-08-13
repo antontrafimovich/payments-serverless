55dfb85f821cebe91c9b33929f9ac18cd33be4c7
Anton Trafimovich <tonytrof@gmail.com>
2023-08-13 23:50:58 +0200

    feat: add select to front.

--- clients/web/src/src/App.tsx
+++ clients/web/src/src/App.tsx
@@ -2,17 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hola Liza Report</h1>
-      <form
-        action={`${import.meta.env.VITE_API_URL as string}/report`}
-        encType="multipart/form-data"
-        method="post"
-      >
-        <input type="file" name="report" />
-        <button type="submit">Send file</button>
-      </form>
-    </>
+    <form
+      action={`${import.meta.env.VITE_API_URL as string}/report`}
+      encType="multipart/form-data"
+      method="post"
+    >
+      <select name="bank" id="bank">
+        <option value="pko">PKO</option>
+        <option value="millenium">Millenium</option>
+      </select>
+      <input type="file" name="report" />
+      <button type="submit">Send file</button>
+    </form>
   );
 }
 
