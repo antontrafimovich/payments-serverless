c182b53fc0dde4aedbec03cf225026a3b55e31fa
Anton Trafimovich <tonytrof@gmail.com>
2023-08-25 20:59:54 +0200

    fix: get rid of an old table.

--- clients/web/src/src/expenses/expenses.tsx
+++ clients/web/src/src/expenses/expenses.tsx
@@ -1,22 +1,8 @@
 import "./expenses.css";
 import "react-data-grid/lib/styles.css";
 
-import { useLayoutEffect, useMemo, useRef, useState } from "react";
-import DataGrid from "react-data-grid";
-import { createPortal } from "react-dom";
-import {
-  AppShell,
-  Button,
-  Container,
-  Flex,
-  Header,
-  Modal,
-  Navbar,
-  Table,
-  TextInput,
-} from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { apiPaths, usePost } from "../shared";
+import { AppShell, Button, Flex, Header, Navbar, Table } from "@mantine/core";
+import { useMemo } from "react";
 
 export type ExpensesProps = {
   info: {
@@ -26,11 +12,6 @@ export type ExpensesProps = {
 };
 
 export const Expenses = ({ info }: ExpensesProps) => {
-  const { post, data } = usePost(apiPaths.map + "/map");
-
-  const [address, setAddress] = useState<string | null>(null);
-  const [type, setType] = useState<string | null>(null);
-
   const [columns, rows] = useMemo(() => {
     const columns = info.headers.map((item) => ({
       key: item,
@@ -49,40 +30,6 @@ export const Expenses = ({ info }: ExpensesProps) => {
 
     return [columns, rows];
   }, [info]);
-
-  const [contextMenuProps, setContextMenuProps] = useState<{
-    rowIdx: number;
-    top: number;
-    left: number;
-  } | null>(null);
-
-  const [opened, { open, close }] = useDisclosure(false);
-
-  const menuRef = useRef<HTMLMenuElement | null>(null);
-
-  const isContextMenuOpen = contextMenuProps !== null;
-
-  console.log(data);
-
-  useLayoutEffect(() => {
-    if (!isContextMenuOpen) return;
-
-    function onClick(event: MouseEvent) {
-      if (
-        event.target instanceof Node &&
-        menuRef.current?.contains(event.target)
-      ) {
-        return;
-      }
-      setContextMenuProps(null);
-    }
-
-    addEventListener("click", onClick);
-
-    return () => {
-      removeEventListener("click", onClick);
-    };
-  }, [isContextMenuOpen]);
 
   const onDownload = () => {
     const data = JSON.stringify(info);
@@ -145,67 +92,6 @@ export const Expenses = ({ info }: ExpensesProps) => {
           })}
         </tbody>
       </Table>
-      <DataGrid
-        columns={columns}
-        rows={rows}
-        onCellContextMenu={({ row }, event) => {
-          event.preventGridDefault();
-          // Do not show the default context menu
-          event.preventDefault();
-          setContextMenuProps({
-            rowIdx: rows.indexOf(row),
-            top: event.clientY,
-            left: event.clientX,
-          });
-        }}
-      />
-      {isContextMenuOpen &&
-        createPortal(
-          <menu
-            ref={menuRef}
-            className="expenses-context-menu"
-            style={
-              {
-                top: contextMenuProps.top,
-                left: contextMenuProps.left,
-              } as unknown as React.CSSProperties
-            }
-          >
-            <li>
-              <button type="button" onClick={open}>
-                Set type
-              </button>
-            </li>
-          </menu>,
-          document.body
-        )}
-
-      <Modal
-        opened={opened}
-        onClose={close}
-        title="Authentication"
-        style={{ left: 0 }}
-      >
-        <TextInput
-          label="Address"
-          data-autofocus
-          placeholder="Enter address"
-          onChange={(el) => setAddress(el.target.value)}
-        />
-        <TextInput
-          label="Type"
-          placeholder="Enter type of the expense"
-          mt="md"
-          onChange={(el) => setType(el.target.value)}
-        />
-        <Button
-          onClick={() => {
-            post(JSON.stringify([{ address, type }]));
-          }}
-        >
-          Insert
-        </Button>
-      </Modal>
     </AppShell>
   );
 };
