4d102b0a02cfa9fa2bd72f83c0f4bb0c12fa524e
Anton Trafimovich <tonytrof@gmail.com>
2023-09-11 12:25:56 +0200

    feat: enhance millenium report parser.

--- backend/parser-service/src/report/mil.ts
+++ backend/parser-service/src/report/mil.ts
@@ -1,11 +1,19 @@
+const getValueFromPayment = (payment: string[]) => {
+  return payment[7] || payment[8];
+};
+
+const getCptyDescription = (payment: string[]) => {
+  return `${payment[5]} ${payment[6]}`;
+};
+
 export const parse = (csv: string[][]) => {
   const [_, ...rows] = csv;
 
   return rows.reduce<{ address: string; date: string; value: string }[]>(
     (result, item) => {
-      const address = item[6];
+      const address = getCptyDescription(item);
       const date = item[1];
-      const value = item[7];
+      const value = getValueFromPayment(item);
 
       if (!address) {
         return result;
