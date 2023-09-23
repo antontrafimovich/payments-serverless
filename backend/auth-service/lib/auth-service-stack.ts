c691636c063b351d376356568d3e3d2eda614b0a
Anton Trafimovich <tonytrof@gmail.com>
2023-09-23 18:23:40 +0200

    feat: add auth-service's stack and bin.

--- backend/auth-service/lib/auth-service-stack.ts
+++ backend/auth-service/lib/auth-service-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as gw from "aws-cdk-lib/aws-apigateway";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 
 export class AuthServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const gateway = new gw.RestApi(this, "AtPaymentsAuthApi", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: gw.Cors.ALL_ORIGINS,
+        allowMethods: gw.Cors.ALL_METHODS,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const authHandler = new lambda.NodejsFunction(this, "AuthHandler", {
+      entry: "src/auth/index.ts",
+      handler: "handler",
+      runtime: Runtime.NODEJS_18_X,
+      environment: {
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+        GOOGLE_CLIENT_SECRET: process.env.NOTION_KEY as string,
+        GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI as string,
+      },
+    });
+
+    const redirectHandler = new lambda.NodejsFunction(this, "RedirectHandler", {
+      entry: "src/redirect/index.ts",
+      handler: "handler",
+      runtime: Runtime.NODEJS_18_X,
+      environment: {
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+        GOOGLE_CLIENT_SECRET: process.env.NOTION_KEY as string,
+        GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI as string,
+      },
+    });
+
+    const authIntegration = new gw.LambdaIntegration(authHandler);
+    const authResource = gateway.root.addResource("auth");
+    authResource.addMethod("GET", authIntegration);
+
+    const redirectIntegration = new gw.LambdaIntegration(redirectHandler);
+    const redirectResource = authResource.addResource("redirect");
+    redirectResource.addMethod("GET", redirectIntegration);
   }
 }
