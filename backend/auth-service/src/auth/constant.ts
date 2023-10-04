1bb6971e51c0747cd8585eb9fe8a915c5fe4131d
Anton Trafimovich <tonytrof@gmail.com>
2023-10-04 20:13:32 +0200

    fix: get rid of unused constants.

--- backend/auth-service/src/auth/constant.ts
+++
@@ -1,9 +0,0 @@
-export const GOOGLE_DRIVE_FILE_SCOPE =
-  "https://www.googleapis.com/auth/drive.file";
-export const GOOGLE_SPREADSHEET_SCOPE =
-  "https://www.googleapis.com/auth/spreadsheets";
-export const GOOGLE_USERINFO_SCOPE =
-  "https://www.googleapis.com/auth/userinfo.profile";
-
-export const GOOGLE_DRIVE_VERSION = "v3";
-export const GOOGLE_SPREADSHEET_VERSION = "v4";
