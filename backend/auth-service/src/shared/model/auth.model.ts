ac0384752bb44759cc102015fb2cbaa88768a9d5
Anton Trafimovich <tonytrof@gmail.com>
2023-10-04 20:14:15 +0200

    feat: add getToken method to ThirdPartyAuthService abstract class.

--- backend/auth-service/src/shared/model/auth.model.ts
+++ backend/auth-service/src/shared/model/auth.model.ts
@@ -6,6 +6,11 @@ export abstract class ThirdPartyAuthService extends AuthService {
   public isThirdParty = true;
 
   public abstract generateUrl(redirectTo: string): Promise<string>;
+
+  public abstract getToken(
+    code: string,
+    redirectTo: string
+  ): Promise<Record<string, any>>;
 }
 
 export abstract class LocalAuthService extends AuthService {
