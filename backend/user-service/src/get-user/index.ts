79eb0d271bf9a0aa786c73ecd38a6dd837554050
Anton Trafimovich <tonytrof@gmail.com>
2023-10-05 13:45:52 +0200

    feat: add basic implementation for get-user handler.

--- backend/user-service/src/get-user/index.ts
+++ backend/user-service/src/get-user/index.ts
@@ -1,78 +1,21 @@
-import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { GoogleUserReceiverService } from "./google.service";
 
-import { createAuthService } from "../shared";
-import {
-  GOOGLE_DRIVE_FILE_SCOPE,
-  GOOGLE_SPREADSHEET_SCOPE,
-  GOOGLE_USERINFO_SCOPE,
-} from "./constant";
+const getReceiver = (type: "google") => {
+  return new GoogleUserReceiverService();
+};
 
 export const handler = async (
-  event: APIGatewayEvent
-): Promise<APIGatewayProxyResult> => {
+  event: Record<string, any>
+): Promise<Record<string, any>> => {
   console.log(event);
 
-  const authService = createAuthService({
-    clientId: process.env.GOOGLE_CLIENT_ID!,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    redirectUris: [`https://${domainName}/prod/auth/redirect`],
-  });
+  const receiver = getReceiver("google");
 
-  const authHeaderValue = event.headers["authorization"];
-
-  if (!authHeaderValue) {
-    console.error("You're not logged in");
-
-    return {
-      statusCode: 401,
-      headers: {
-        "Content-type": "text/plain",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "*",
-      },
-      body: "You're not logged in",
-    };
-  }
-
+  const authHeaderValue = event.headers["Authorization"];
   const [, token] = authHeaderValue.split("Bearer ");
 
-  const decodedToken = decodeURIComponent(token);
-
-  if (!token) {
-    console.error("Some trouble with token");
-    return {
-      statusCode: 400,
-      headers: {
-        "Content-type": "text/plain",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "*",
-      },
-      body: "Some trouble with token",
-    };
-  }
-
   try {
-    oAuth2Client.setCredentials(JSON.parse(atob(decodedToken)));
-  } catch (err) {
-    console.log(err);
-    return {
-      statusCode: 400,
-      headers: {
-        "Content-Type": "text/plain",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "*",
-      },
-      body: `Google OAuth2.0 error: ${JSON.stringify(err)}`,
-    };
-  }
-
-  const oath2 = google.oauth2({ version: "v2", auth: oAuth2Client });
-
-  try {
-    const userInfo = await oath2.userinfo.get();
+    const userInfo = await receiver.getUser(token);
 
     return {
       statusCode: 200,
@@ -85,16 +28,16 @@ export const handler = async (
       body: JSON.stringify(userInfo),
     };
   } catch (err) {
-    console.log("User info getting error:", err);
+    console.error("User Service: getUser handler error:", err);
     return {
-      statusCode: 500,
+      statusCode: 400,
       headers: {
-        "Content-Type": "text/plain",
+        "Content-type": "text/plain",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Methods": "*",
       },
-      body: `User info getting error: ${JSON.stringify(err)}`,
+      body: `User Service: getUser handler error: ${err}`,
     };
   }
 };
