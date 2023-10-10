eafd785547ea0ab19c0c1ebd33e573f54fa5d380
Anton Trafimovich <tonytrof@gmail.com>
2023-10-10 23:53:38 +0200

    feat: add createSheet method to auth.service.

--- backend/google-service/src/shared/lib/service/auth.service.ts
+++ backend/google-service/src/shared/lib/service/auth.service.ts
@@ -32,5 +32,28 @@ export const createAuthService = (credentials: {
 
       return oath2.userinfo.get();
     },
+    createSheet: async (name: string) => {
+      const drive = google.drive({ version: "v3", auth: oAuth2Client });
+
+      const googleSheetCreateRepsponse = await drive.files.create({
+        requestBody: {
+          name,
+          mimeType: "application/vnd.google-apps.spreadsheet",
+        },
+      });
+
+      const sheets = google.sheets({ version: "v4", auth: oAuth2Client });
+
+      try {
+        const spreadsheet = await sheets.spreadsheets.batchUpdate({
+          spreadsheetId: googleSheetCreateRepsponse.data.id!,
+        });
+
+        // console.log(`Spreadsheet ID: ${spreadsheet.data.spreadsheetId}`);
+      } catch (err) {
+        // TODO (developer) - Handle exception
+        throw err;
+      }
+    },
   };
 };
