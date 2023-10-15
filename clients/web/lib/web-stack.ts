8444b474892b9836fed92676ecfe01bb73bc4bbe
Anton Trafimovich <tonytrof@gmail.com>
2023-10-15 23:51:49 +0200

    feat: add some restructuring to web-stack.

--- clients/web/lib/web-stack.ts
+++ clients/web/lib/web-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import { Construct } from "constructs";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
