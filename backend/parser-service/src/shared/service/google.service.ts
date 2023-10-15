d85eccc967e4088dadd720f3581733228f2023b2
Anton Trafimovich <tonytrof@gmail.com>
2023-10-15 23:48:14 +0200

    feat: add google service with createStorage method.

---
+++ backend/parser-service/src/shared/service/google.service.ts
@@ -0,0 +1,40 @@
+import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
+
+export const createGoogleService = () => {
+  const client = new LambdaClient({ region: process.env.REGION });
+
+  return {
+    createStorage: async (
+      name: string,
+      token: string,
+      data: string
+    ): Promise<string> => {
+      const command = new InvokeCommand({
+        FunctionName: process.env.GOOGLE_CREATE_SHEET_FUNCTION_NAME as string,
+        InvocationType: "RequestResponse",
+        Payload: JSON.stringify({ body: { fileName: name, token, data } }),
+      });
+
+      let response;
+      try {
+        console.log("Command has been invoked");
+        response = await client.send(command);
+      } catch (err) {
+        console.error("Google Lambda Invokation error:", err);
+        throw err;
+      }
+
+      try {
+        console.log("Response has been received:", response);
+        const rawResponse = new TextDecoder("utf-8").decode(response.Payload);
+
+        return JSON.parse(rawResponse);
+      } catch (err) {
+        console.log("Decoding Google Lambda Create Sheet Response error:", err);
+        throw err;
+      }
+    },
+    patchStorage: async () => {},
+    appendDataToStorage: async () => {},
+  };
+};
