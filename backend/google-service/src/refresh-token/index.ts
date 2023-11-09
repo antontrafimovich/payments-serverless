d454d8443686199871f4e73e99cb838627a087e2
Anton Trafimovich <tonytrof@gmail.com>
2023-11-09 23:23:13 +0100

    feat: GoogleService - refactor refresh token handler.

--- backend/google-service/src/refresh-token/index.ts
+++ backend/google-service/src/refresh-token/index.ts
@@ -1,4 +1,12 @@
-import { createGoogleService } from "../shared";
+import { createGoogleClient } from "../shared/lib/service/google-client.service";
+import { NO_REDIRECT_URI_ERROR, NO_TOKEN_ERROR } from "./lib/error";
+
+const parseToken = (token: string) => {
+  const decodedBase64Token = decodeURIComponent(token);
+  const decodedToken = atob(decodedBase64Token);
+
+  return JSON.parse(decodedToken);
+};
 
 export const handler = async (event: {
   body: { redirectUri: string; token: string };
@@ -7,18 +15,26 @@ export const handler = async (event: {
 
   const { token, redirectUri } = event.body;
 
-  const googleService = createGoogleService({
+  if (!redirectUri) {
+    throw NO_REDIRECT_URI_ERROR;
+  }
+
+  if (!token) {
+    throw NO_TOKEN_ERROR;
+  }
+
+  const client = createGoogleClient({
     clientId: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     redirectUri: `https://${redirectUri}`,
   });
 
-  const decodedToken = decodeURIComponent(token);
+  const creds = parseToken(token);
 
-  googleService.setToken(decodedToken);
+  client.setCredentials(creds);
 
   try {
-    const { credentials } = await googleService.refreshToken();
+    const { credentials } = await client.refreshAccessToken();
 
     console.log("Result tokens:", credentials);
 
---
+++ backend/google-service/src/refresh-token/lib/error.ts
@@ -0,0 +1,4 @@
+export const NO_REDIRECT_URI_ERROR = new Error(
+  "Redirect uri hasn't been provided"
+);
+export const NO_TOKEN_ERROR = new Error("Token hasn't been provided");
