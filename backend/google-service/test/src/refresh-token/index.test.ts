2f9b8ffdc17c09a2de1b0b0e3756171cf30ba5d4
Anton Trafimovich <tonytrof@gmail.com>
2023-11-09 23:23:33 +0100

    feat: GoogleService - add test to refresh token handler.

---
+++ backend/google-service/test/src/refresh-token/index.test.ts
@@ -0,0 +1,56 @@
+import {
+  NO_REDIRECT_URI_ERROR,
+  NO_TOKEN_ERROR,
+} from "../../../src/refresh-token/lib/error";
+import { handler } from "./../../../src/refresh-token";
+
+jest.mock("./../../../src/shared/lib/service/google-client.service.ts", () => {
+  return {
+    createGoogleClient: jest.fn(function () {
+      return {
+        setCredentials: jest.fn(function () {
+          console.log("Credentials have been set");
+        }),
+
+        refreshAccessToken: jest.fn(function () {
+          return { credentials: "TEST_TOKEN" };
+        }),
+      };
+    }),
+  };
+});
+
+describe("Refresh Token functionality", () => {
+  it("should throw error if redirectUri is not provided", async () => {
+    expect.assertions(1);
+
+    const INPUT_PARAMS = { body: { token: "fake" } } as any;
+
+    await expect(handler(INPUT_PARAMS)).rejects.toThrowError(
+      NO_REDIRECT_URI_ERROR
+    );
+  });
+
+  it("should throw error if token is not provided", async () => {
+    expect.assertions(1);
+
+    const INPUT_PARAMS = { body: { redirectUri: "fake" } } as any;
+
+    await expect(handler(INPUT_PARAMS)).rejects.toThrowError(NO_TOKEN_ERROR);
+  });
+
+  it("should return new token", async () => {
+    expect.assertions(1);
+
+    const INPUT_PARAMS = {
+      body: {
+        redirectUri: "redirect",
+        token: encodeURIComponent(btoa('{"value": "token"}')),
+      },
+    } as any;
+
+    await expect(handler(INPUT_PARAMS)).resolves.toBe(
+      btoa(JSON.stringify("TEST_TOKEN"))
+    );
+  });
+});
